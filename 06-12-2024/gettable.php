f56f16dbb0f5d143c3b65eef256fad9e55256089
danudilanjalee <139612457+danudilanjalee@users.noreply.github.com>
2024-12-08 13:19:56 +0000

    gettable file created

---
+++ 06-12-2024/gettable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+//get the db connection file
+
+require_once 'dbconf.php';
+require_once 'myfunc.php';
+
+
+book($connect);
+
+?>
+
+
+
+</body>
+</html>
