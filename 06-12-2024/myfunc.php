6cf7ce55bcc13afb9014c6be8d4ec1542de83971
danudilanjalee <139612457+danudilanjalee@users.noreply.github.com>
2024-12-08 13:39:26 +0000

    save changes in my function file

--- 06-12-2024/myfunc.php
+++ 06-12-2024/myfunc.php
@@ -1,55 +1,62 @@
 <?php
 //get the database connection file
 require_once 'dbconf.php';
-function PrintTable($tableName,$connect){
-try{
-	//Query
-	$sql = "SELECT * FROM $tableName";
-
-	//execute the query (call variable,query)
-	$result = mysqli_query($connect,$sql);
-
-	if (mysqli_num_rows($result)>0) {
-		//fetch the data from rows
-
-		echo "<table border=1> ";
-
-	$col = mysqli_fetch_fields($result);
-	//print the column
-	echo "<tr>";
-	foreach ($col as $value) {
-		//return an object
-		//print_r($value)
-			echo "<td>".$value->name."</td>";
-		}
-		echo "</tr>";
-		while($row = mysqli_fetch_assoc($result)){
-			//print the data in table format
-
-		echo "<tr>";
-		foreach ($row as $key => $value) {
-			echo "<td>$value</td>";
-		}
-		echo "</tr>";
-		}
-		echo "</table>";
-	} 
-	else{
-		echo "No results"; //table is empty
-	}
-
-	}
-
-
-catch(Exception $e){
-	die($e->getMessage());
-}
-}
  //GET DATA FROM DB
- function Searchestudent($name,$connect){
+ function student($connect){
             try{
             
-                $sql = "SELECT * FROM book where name like '%$name%' ";
+                $sql = "SELECT id,name FROM  student";
+            
+            
+                $result = mysqli_query($connect,$sql);
+            
+                if (mysqli_num_rows($result)>0) {
+                
+            
+                    echo "<table border=1> ";
+            
+                $col = mysqli_fetch_fields($result);
+            
+                echo "<tr>";
+                foreach ($col as $value) {
+                    
+                        echo "<td>".$value->name."</td>";
+                    }
+
+                    echo "<td> View details </td>";
+                    echo "</tr>";
+                    while($row = mysqli_fetch_assoc($result)){ 
+                    echo "<tr>";
+                    foreach ($row as $key => $value) {
+                        echo "<td>$value</td>";
+                    }
+                    $id=$row['id'];
+                    //query string
+                    echo "<td><a href='printtable.php? id=$id '> View </a> </td>";
+                    echo "</tr>";
+                    
+                    }
+
+                    echo "</table>";
+                } 
+                else{
+                    echo "No results"; 
+                }
+            
+                }
+            
+            
+            catch(Exception $e){
+                die($e->getMessage());
+            }
+        }
+
+
+
+         function studentdetails($id,$connect){
+            try{
+            
+                $sql = "SELECT * FROM  student where id= $id";
             
             
                 $result = mysqli_query($connect,$sql);
