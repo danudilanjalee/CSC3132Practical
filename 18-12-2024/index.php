6a753a10939882299da9843dbdd4fc11eb0ff903
danudilanjalee <139612457+danudilanjalee@users.noreply.github.com>
2024-12-20 09:28:51 +0000

    created main file as index

---
+++ 18-12-2024/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="gettable.php">Gettable</a><br>
+    <a href="printtable.php">printtable</a><br>
+
+    
+</body>
+</html>
